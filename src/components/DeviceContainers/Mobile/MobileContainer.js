ac3203cab432fa35b23d273cc6ebd54f27cbf4ba
Brendan <eggers.brendan@gmail.com>
2020-06-04 18:07:45 -0400

    Going to have to create these in MUI components repo

--- src/components/DeviceContainers/Mobile/MobileContainer.js
+++ src/components/DeviceContainers/Mobile/MobileContainer.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    mobileProjectContainer: {
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        border: `1px solid ${theme.palette.common.black}`,
+        height: "200px",
+        width: "20%"
+    }
+}));
 
 const MobileContainer = ({ mobile_image }) => {
+    const classes = useStyles();
     return <div></div>
+    // return <div className={classes.mobileProjectContainer} style={{ backgroundImage: `url(${mobile_image})`}} />
 }
 
 export default MobileContainer;
