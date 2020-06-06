f5bb81fe89744d65e4074a79fe2266590f6f2463
Brendan <eggers.brendan@gmail.com>
2020-06-06 09:54:49 -0400

    Adding in transistions for job listings

---
+++ src/components/JobListing/JobListing.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Grow from "@material-ui/core/Grow";
+import Moment from "react-moment";
+Moment.globalFormat = "MMM YYYY";
+
+const JobListing = ({ jobData: job, index }) => {
+  const companySplit = job.company.split("&").join("\n");
+  return (
+    <Grow in={true} timeout={index === 0 ? 500 : index * 500}>
+      <Grid container spacing={2} item xs={12} key={job._id}>
+        <Grid item xs={12} sm={5}>
+          <Typography
+            style={{ whiteSpace: "pre-line" }}
+            variant="h5"
+            color="primary"
+          >
+            {companySplit}
+          </Typography>
+          <Typography variant="h6">{job.job_title}</Typography>
+          <Typography variant="body1">
+            <Moment parse="YYYY-MM-DD">{job.start_date}</Moment>
+            {" - "}
+            <Moment parse="YYYY-MM-DD">{job.end_date}</Moment>
+          </Typography>
+          <Typography variant="body1">{job.location}</Typography>
+        </Grid>
+        <Grid item xs={12} sm={7}>
+          <Typography variant="body1">{job.description}</Typography>
+        </Grid>
+      </Grid>
+    </Grow>
+  );
+};
+
+export default JobListing;
