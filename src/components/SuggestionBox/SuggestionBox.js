924679209509700b3b7837a19f7bb85b73f89642
Brendan <eggers.brendan@gmail.com>
2020-06-16 12:05:44 -0400

    Adding Twilio to Suggestion Box

--- src/components/SuggestionBox/SuggestionBox.js
+++ src/components/SuggestionBox/SuggestionBox.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Filters from "../Filters/Filters";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles((theme) => ({}));
+const API_URL = window.API_URL;
 
 const SuggestionBox = () => {
+  const [thanks, setThanks] = useState(false);
+  const [error, setError] = useState(false);
   const [inputVal, setInputVal] = useState("");
   const [selectedFilter, setSelectedFilter] = useState(null);
-  const filterArray = ["React", "Python", "Raspberry Pi", "Else"];
+  const filterArray = ["React", "Python", "Raspberry Pi", "Other"];
 
   const getRandomIdea = () => {
     const ideaArray = [
@@ -25,7 +25,7 @@ const SuggestionBox = () => {
   };
 
   const filterClick = (val) => {
-    if (val == selectedFilter) setSelectedFilter(null);
+    if (val === selectedFilter) setSelectedFilter(null);
     else setSelectedFilter(val);
   };
 
@@ -34,28 +34,55 @@ const SuggestionBox = () => {
     setInputVal(value);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    if (!selectedFilter || !inputVal) {
+      setError(true);
+      return;
+    }
+
+    const req = await fetch(`${API_URL}/twilio`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: selectedFilter,
+        idea: inputVal,
+      }),
+    });
+    const res = await req.json();
+    if (res.success) setThanks(true);
+  };
 
   return (
     <Grid container direction="column" alignItems="center" justify="center">
-      <Typography variant="h5">
-        What would you like me to build next?
-      </Typography>
-      <Filters
-        selectedFilter={selectedFilter}
-        setSelectedFilter={filterClick}
-        filters={filterArray}
-      />
-      <TextField
-        value={inputVal}
-        onChange={handleChange}
-        label="Brief Description"
-        placeholder={`Brief Description (eg: ${getRandomIdea()})`}
-        variant="outlined"
-        fullWidth
-        style={{ marginBottom: "1rem" }}
-      />
-      <Button variant="contained">Send Idea</Button>
+      {!thanks ? (
+        <>
+          <Typography variant="h5" gutterBottom>
+            What would you like me to build next?
+          </Typography>
+          {error && <Typography color="error">Please add some info</Typography>}
+          <Filters
+            selectedFilter={selectedFilter}
+            setSelectedFilter={filterClick}
+            filters={filterArray}
+          />
+          <TextField
+            value={inputVal}
+            onChange={handleChange}
+            label="Brief Description"
+            placeholder={`Brief Description (eg: ${getRandomIdea()})`}
+            variant="outlined"
+            fullWidth
+            style={{ marginBottom: "1rem" }}
+          />
+          <Button onClick={handleSubmit} variant="contained">
+            Send Idea
+          </Button>
+        </>
+      ) : (
+        <Typography variant="h3">Thanks for your input!</Typography>
+      )}
     </Grid>
   );
 };
