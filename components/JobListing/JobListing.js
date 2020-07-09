8b12bb660665cc961609de64675d5011a119bff7
Brendan <eggers.brendan@gmail.com>
2020-07-09 09:47:34 -0400

    Adding Growth Bars

--- components/JobListing/JobListing.js
+++ components/JobListing/JobListing.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Grow from "@material-ui/core/Grow";
 import Moment from "react-moment";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const useStyles = makeStyles((theme) => ({
+  list: {
+    paddingTop: "0",
+  },
+}));
 
 const JobListing = ({ jobData: job, index }) => {
   const companySplit = job.company.split("&").join("\n");
+  const { list } = useStyles();
   return (
     <Grow in={true} timeout={(index + 1) * 500}>
       <Grid container spacing={2} item xs={12} key={job._id}>
-        <Grid item xs={12} sm={5}>
+        <Grid item xs={12} sm={4}>
           <Typography
             style={{ whiteSpace: "pre-line" }}
             variant="h5"
@@ -25,8 +36,14 @@ const JobListing = ({ jobData: job, index }) => {
           </Typography>
           <Typography variant="body1">{job.location}</Typography>
         </Grid>
-        <Grid item xs={12} sm={7}>
-          <Typography variant="body1">{job.description}</Typography>
+        <Grid item xs={12} sm={8}>
+          <List dense className={list}>
+            {job.points.map((point, index) => (
+              <ListItem key={`${index}_point`}>
+                <ListItemText primary={point} />
+              </ListItem>
+            ))}
+          </List>
         </Grid>
       </Grid>
     </Grow>
--- pages/lab.js
+++ pages/lab.js
@@ -1,14 +1,96 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 import Fade from "@material-ui/core/Fade";
 import Grid from "@material-ui/core/Grid";
 import SuggestionBox from "../components/SuggestionBox/SuggestionBox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+const BorderLinearProgress = withStyles((theme) => ({
+  root: {
+    height: 10,
+    borderRadius: 5,
+    width: 100,
+    marginLeft: 10,
+  },
+  colorPrimary: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
+  },
+  bar: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.primary.main,
+  },
+}))(LinearProgress);
 
 const Lab = () => {
+  const learningList = [
+    {
+      name: "React",
+      progress: 90,
+    },
+    {
+      name: "Redux",
+      progress: 90,
+    },
+    {
+      name: "Next.js",
+      progress: 70,
+    },
+    {
+      name: "MongoDB",
+      progress: 70,
+    },
+    {
+      name: "Flask",
+      progress: 55,
+    },
+    {
+      name: "Docker",
+      progress: 25,
+    },
+    {
+      name: "Redwood.js",
+      progress: 10,
+    },
+    {
+      name: "GraphQL",
+      progress: 10,
+    },
+    {
+      name: "Redis",
+      progress: 0,
+    },
+  ];
   return (
     <Fade in={true} timeout={250}>
       <Grid container alignItems="center" justify="center">
         <Grid item xs={12} md={8}>
           <SuggestionBox />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          md={8}
+          alignItems="center"
+          direction="column"
+        >
+          <Typography variant="h6">How I'm Growing</Typography>
+          <List>
+            {learningList.map((tech) => (
+              <ListItem key={tech.name.toLowerCase()}>
+                <ListItemText primary={tech.name} />
+                <BorderLinearProgress
+                  variant="determinate"
+                  value={tech.progress}
+                />
+              </ListItem>
+            ))}
+          </List>
         </Grid>
       </Grid>
     </Fade>
