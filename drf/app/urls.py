d76e2fa980936cc3f7cca58a35d410f5b53e986b
eggdev <eggers.brendan@gmail.com>
2021-04-28 09:57:36 -0400

    Updating bucket to be for users

--- drf/app/urls.py
+++ drf/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from app.projects import views
+from app.users import views
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
--- drf/app/users/views.py
+++ drf/app/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
-from app.projects.serializers import UserSerializer, GroupSerializer
+from app.users.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
