b9ce92f0273d2868f47b4d7fb3fe494aa155ad75
eggdev <eggers.brendan@gmail.com>
2021-04-29 16:04:36 -0400

    Successful addition of technologies model

--- drf/portfolio/models/project.py
+++ drf/portfolio/models/project.py
@@ -6,8 +6,9 @@ from django.db import models
 
 class Project(models.Model):
     created_date = models.DateField(default=datetime.date.today)
-    tagline = models.CharField(max_length=256, blank=True, default="")
-    title = models.CharField(max_length=100, blank=True, default="")
+    technologies = models.JSONField(default=["react"])
+    tagline = models.CharField(max_length=256, default="")
+    title = models.CharField(max_length=100, default="")
 
     class Meta:
         ordering = ["title"]
